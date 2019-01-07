03a81debea7a567576d4f305de3603b8c5084ec9
fural630 <312651213@qq.com>
2019-01-07 11:28:16 +0800

    modfiy index.html

---
+++ xhz-front/src/main/resources/static/js/libs/menu.js
@@ -0,0 +1,14 @@
+var menu = {
+	menuList : [{
+		"F_ModuleId": "1",
+		"F_ParentId": "0",
+		"F_FullName": "系统管理",
+		"F_Icon": "fa fa-desktop"
+	},{
+		"F_ModuleId": "2",
+		"F_ParentId": "1",
+		"F_FullName": "用户管理",
+		"F_Icon": "fa fa-user",
+		"F_UrlAddress": "/sys/user.html",
+	}]
+}
