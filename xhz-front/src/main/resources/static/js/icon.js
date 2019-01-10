6912c3f385a88335b592fc47a8a643ed81eedadc
fural630 <312651213@qq.com>
2019-01-10 15:59:16 +0800

    finish icon

--- xhz-front/src/main/resources/static/js/icon.js
+++ xhz-front/src/main/resources/static/js/icon.js
@@ -1,46 +1,64 @@
 var vm = new Vue({
-	el : '#app',
-	data : {
-		q : {
-			name : ''
+	el: '#app',
+	data: {
+		q: {
+			name: ''
 		},
-		commonIcon : {
-			iconName : '',
-			space : 0,
-			zoom : 1,
-			rotate : 0
+		commonIcon: {
+			iconName: '',
+			space: 0,
+			zoom: 1,
+			rotate: 0
 		},
-		iconItems : iconLib,
-		hasResult : false,
-		template : '<i class="fa fa-{0}{1}{2}"></i>{3}{4}',
-		iconCode : '',
-		iconShow : '',
-		iconCopyCode : '',
-		iconShow90 : '',
-		iconShow180 : '',
-		iconShow270 : '',
-		iconShowHorizontal : '',
-		iconShowVertical : '',
-		windowIndex : 99
+		btnIcon: {
+			iconName: '',
+			space: 0,
+			zoom: 4,
+			color: '',
+			style: '',
+			rotate: 0
+		},
+		iconItems: [],
+		hasResult: false,
+		iconTempate: '<i class="fa fa-{0}{1}{2}"></i>{3}{4}',
+		iconBtnTemplate: `<el-button{5}{6}{7}>
+  <i class="fa fa-{0}{1}{2}"></i>{3}{4}
+</el-button>`,
+		iconBtnShowTemplate: '<button class="el-button{5}{6}{7}"><i class="fa fa-{0}{1}{2}"></i>{3}{4}</button>',
+		template: '',
+		iconCode: '',
+		iconShow: '',
+		iconCopyCode: '',
+		btnIconShow: '',
+		btnIconCode: '',
+		iconShow90: '',
+		iconShow180: '',
+		iconShow270: '',
+		iconShowHorizontal: '',
+		iconShowVertical: '',
+		windowIndex: 99,
+		activeTab: "1"
 	},
-	methods : {
-		query : function() {
+	methods: {
+		query: function() {
 			var searchIcon = [];
 			for (var i = 0; i < iconLib.length; i++) {
 				var obj = iconLib[i];
 				var label = iconLib[i].label;
 				var icon = iconLib[i].icon;
-				if (icon.toLowerCase().indexOf(this.q.name.toLowerCase()) >= 0
-						|| label.indexOf(this.q.name) >= 0) {
+				if (icon.toLowerCase().indexOf(this.q.name.toLowerCase()) >= 0 ||
+					label.indexOf(this.q.name) >= 0) {
 					searchIcon.push(obj);
 				}
 			}
 			this.iconItems = searchIcon;
 		},
-		copyIcon : function(iconText) {
+		copyIcon: function(iconText) {
+			this.iconCode = iconText;
+			this.template = this.iconTempate;
 			var clipboard = new ClipboardJS('.copyBtn', {
-				text : function () {
-					return this.parseCode(0, iconText);
+				text: function() {
+					return vm.parseCode(0, iconText);
 				}
 			});
 			clipboard.on('success', function(e) {
@@ -52,10 +70,10 @@ var vm = new Vue({
 				console.log(e);
 			});
 		},
-		copyConfigIcon : function () {
+		copyConfigIcon: function() {
 			layer.close(this.windowIndex);
 			var clipboard = new ClipboardJS('.configCopyBtn', {
-				text : function () {
+				text: function() {
 					return vm.iconCopyCode;
 				}
 			});
@@ -67,82 +85,112 @@ var vm = new Vue({
 				console.log(e);
 			});
 		},
-		configCopy : function (iconText) {
-			var text = this.template;
+		configCopy: function(iconText) {
 			this.iconCode = iconText;
-			this.iconShow = this.parseCode(0, iconText);
-			this.iconCopyCode = this.parseCode(0, iconText);
+			this.changeIconDate();
 			this.windowIndex = openWindow({
-                title: "高级复制图标配置",
-                area: ['720px', '580px'],
-                content: jQuery("#configCopyDialog"),
-                btn: ['关闭'],
-                yes: function (index) {
-                    layer.close(index);
-                },
-                success : function (layero, index) {
-                	vm.resetCommonConfig();
-                }
-            });
+				title: "高级复制图标配置",
+				area: ['760px', '620px'],
+				content: jQuery("#configCopyDialog"),
+				btn: ['关闭'],
+				yes: function(index) {
+					layer.close(index);
+				},
+				success: function(layero, index) {
+					vm.resetConfig();
+				}
+			});
 		},
-		parseCode : function (index, content) {
-			var result = this.template.replace("{0}", this.iconCode);
-			result = result.replace("{"+index+"}", content);
-			return this.cleanTemplate(result);
-		},
-		cleanTemplate : function (result) {
-			for (var i = 0 ; i < 5; i++) {
-				result = result.replace("{"+i+"}", "");
+		changeIconDate: function() {
+			if (this.activeTab === "0") {
+				this.template = this.iconTempate;
+				this.iconShow = this.parseCode(0, this.iconCode);
+			} else if (this.activeTab === "1") {
+				this.btnIconShow = this.parseTemplate(0, this.iconCode, this.iconBtnShowTemplate);
+				this.btnIconShow = this.cleanTemplate(this.btnIconShow);
+				this.template = this.iconBtnTemplate;
+				this.btnIconCode = this.parseCode(0, this.iconCode);
+				this.iconShow = this.parseTemplate(0, this.iconCode, this.iconTempate);
+				this.iconShow = this.cleanTemplate(this.iconShow);
 			}
-			return result;
-		},
-		parseTemplate : function (index, content, templateCode) {
-			return templateCode.replace("{"+index+"}", content);
-		},
-		setRotate : function (rotateType) {
-			this.commonIcon.rotate = rotateType;
-		},
-		resetCommonConfig : function () {
-			this.commonIcon = {
-				iconName : '',
-				space : 0,
-				zoom : 1,
-				rotate : 0
-			}
-		},
-		handleReset : function(name) {
-			handleResetForm(vm, name);
-			this.iconItems = iconLib;
-		}
-	},
-	watch : {
-		iconItems : function() {
-			this.hasResult = this.iconItems.length == 0 ? true : false;
-			vm.copyIcon();
-		},
-		iconCode : function () {
+			this.iconCopyCode = this.iconShow;
 			this.iconShow90 = this.parseCode(1, " fa-rotate-90");
 			this.iconShow180 = this.parseCode(1, " fa-rotate-180");
 			this.iconShow270 = this.parseCode(1, " fa-rotate-270");
 			this.iconShowHorizontal = this.parseCode(1, " fa-flip-horizontal");
 			this.iconShowVertical = this.parseCode(1, " fa-flip-vertical");
 		},
+		parseCode: function(index, content) {
+			var result = this.template.replace("{0}", this.iconCode);
+			result = result.replace("{" + index + "}", content);
+			return this.cleanTemplate(result);
+		},
+		cleanTemplate: function(result) {
+			for (var i = 0; i < 10; i++) {
+				result = result.replace("{" + i + "}", "");
+			}
+			return result;
+		},
+		parseTemplate: function(index, content, templateCode) {
+			return templateCode.replace("{" + index + "}", content);
+		},
+		setRotate: function(rotateType) {
+			this.commonIcon.rotate = rotateType;
+		},
+		setBtnRotate: function(rotateType) {
+			this.btnIcon.rotate = rotateType;
+		},
+		setColor: function(colorType) {
+			this.btnIcon.color = colorType;
+		},
+		setStyle: function(styleType) {
+			this.btnIcon.style = styleType;
+		},
+		resetConfig: function() {
+			this.commonIcon = {
+				iconName: '',
+				space: 0,
+				zoom: 1,
+				rotate: 0
+			};
+			this.btnIcon = {
+				iconName: '',
+				space: 0,
+				zoom: 4,
+				color: '',
+				style: '',
+				rotate: 0
+			};
+		},
+		handleReset: function(name) {
+			handleResetForm(vm, name);
+			this.loadAllIcon();
+		},
+		loadAllIcon: function() {
+			this.iconItems = iconLib;
+		}
+	},
+	watch: {
+		iconItems: function() {
+			this.hasResult = this.iconItems.length == 0 ? true : false;
+			vm.copyIcon();
+		},
 		commonIcon: {
-		　　　　handler(newObj, oldObj) {
+			handler(newObj, oldObj) {
 				var iName = newObj.iconName;
 				var iSpace = newObj.space;
 				var iZoom = newObj.zoom;
 				var iRotate = newObj.rotate;
-				var resultCode = this.parseTemplate(0, this.iconCode, this.template);
+				var resultCode = this.parseTemplate(0, this.iconCode, this.iconTempate);
 				resultCode = this.parseTemplate(4, iName, resultCode);
 				var spaceStr = "";
-				for (var i = 0 ; i < iSpace; i++) {
+				for (var i = 0; i < iSpace; i++) {
 					spaceStr += "&nbsp;"
 				}
 				resultCode = this.parseTemplate(3, spaceStr, resultCode);
 				var zoomStr = "";
 				if (iZoom > 1) {
-					zoomStr = " fa-"+iZoom+"x";
+					zoomStr = " fa-" + iZoom + "x";
 				}
 				resultCode = this.parseTemplate(2, zoomStr, resultCode);
 				if (iRotate == 1) {
@@ -158,12 +206,98 @@ var vm = new Vue({
 				}
 				resultCode = this.cleanTemplate(resultCode);
 				this.iconCopyCode = resultCode;
-		　　　　},
-		　　　　deep: true
-	　　}
+			},
+			deep: true
+		},
+		btnIcon: {
+			handler(newObj, oldObj) {
+				var iName = newObj.iconName;
+				var iSpace = newObj.space;
+				var iZoom = newObj.zoom;
+				var iColor = newObj.color;
+				var iStyle = newObj.style;
+				var iRotate = newObj.rotate;
+				var resultCode = this.parseTemplate(0, this.iconCode, this.iconBtnTemplate);
+				resultCode = this.parseTemplate(4, iName, resultCode); //文字
+
+				var spaceStr = "";
+				for (var i = 0; i < iSpace; i++) {
+					spaceStr += "&nbsp;"
+				}
+				resultCode = this.parseTemplate(3, spaceStr, resultCode); //空格
+
+				var zoomStr = "";
+				var zoomShowStr = "";
+				if (iZoom == 1) {
+					zoomStr = ' size="mini"';
+					zoomShowStr = ' el-button--mini';
+				} else if (iZoom == 2) {
+					zoomStr = ' size="small"';
+					zoomShowStr = ' el-button--small';
+				} else if (iZoom == 3) {
+					zoomStr = ' size="medium"';
+					zoomShowStr = ' el-button--medium';
+				}
+				resultCode = this.parseTemplate(5, zoomStr, resultCode);
+
+				var colorStr = "";
+				var colorShowStr = "";
+				if (iColor != "") {
+					colorStr = ' type="' + iColor + '"';
+					colorShowStr = ' el-button--' + iColor;
+				}
+				resultCode = this.parseTemplate(6, colorStr, resultCode);
+
+				var styleStr = "";
+				var styleShowStr = "";
+				if (iStyle != "") {
+					styleStr = ' ' + iStyle;
+					styleShowStr = ' is-' + iStyle;
+				}
+				resultCode = this.parseTemplate(7, styleStr, resultCode);
+
+				if (iRotate == 1) {
+					resultCode = this.parseTemplate(1, " fa-rotate-90", resultCode);
+				} else if (iRotate == 2) {
+					resultCode = this.parseTemplate(1, " fa-rotate-180", resultCode);
+				} else if (iRotate == 3) {
+					resultCode = this.parseTemplate(1, " fa-rotate-270", resultCode);
+				} else if (iRotate == 4) {
+					resultCode = this.parseTemplate(1, " fa-flip-horizontal", resultCode);
+				} else if (iRotate == 5) {
+					resultCode = this.parseTemplate(1, " fa-flip-vertical", resultCode);
+				}
+
+				resultCode = this.cleanTemplate(resultCode);
+				this.btnIconCode = resultCode;
+				this.iconCopyCode = resultCode;
+
+				var resultShowCode = this.parseTemplate(0, this.iconCode, this.iconBtnShowTemplate);
+				resultShowCode = this.parseTemplate(4, iName, resultShowCode); //文字
+				resultShowCode = this.parseTemplate(3, spaceStr, resultShowCode); //空格
+				resultShowCode = this.parseTemplate(5, colorShowStr, resultShowCode); //颜色
+				resultShowCode = this.parseTemplate(6, zoomShowStr, resultShowCode); //大小
+				resultShowCode = this.parseTemplate(7, styleShowStr, resultShowCode); //样式
+
+				if (iRotate == 1) {
+					resultShowCode = this.parseTemplate(1, " fa-rotate-90", resultShowCode);
+				} else if (iRotate == 2) {
+					resultShowCode = this.parseTemplate(1, " fa-rotate-180", resultShowCode);
+				} else if (iRotate == 3) {
+					resultShowCode = this.parseTemplate(1, " fa-rotate-270", resultShowCode);
+				} else if (iRotate == 4) {
+					resultShowCode = this.parseTemplate(1, " fa-flip-horizontal", resultShowCode);
+				} else if (iRotate == 5) {
+					resultShowCode = this.parseTemplate(1, " fa-flip-vertical", resultShowCode);
+				}
+
+				this.btnIconShow = resultShowCode;
+				this.btnIconShow = this.cleanTemplate(this.btnIconShow);
+			},
+			deep: true
+		}
 	},
-	created : function () {
-//		this.configCopy('camera-retro');
+	mounted: function() {
+		this.loadAllIcon();
 	}
 });
-
