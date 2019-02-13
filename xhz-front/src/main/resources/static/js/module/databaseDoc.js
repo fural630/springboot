2a868d52ad1a2196a6326f378f42fc2ecbd93e5a
fural630 <312651213@qq.com>
2019-02-13 18:34:48 +0800

    数据库文档功能优化

--- xhz-front/src/main/resources/static/js/module/databaseDoc.js
+++ xhz-front/src/main/resources/static/js/module/databaseDoc.js
@@ -1,22 +1,22 @@
 var vm = new Vue({
-	el : '#app',
-	data : {
-		moduleName : 'databaseDoc',
-		baseUrl : '/develop/databaseDocs',
-		q : {
-			id : ''
+	el: '#app',
+	data: {
+		moduleName: 'databaseDoc',
+		baseUrl: '/develop/databaseDocs',
+		q: {
+			id: ''
 		},
-		datasourceItem : [],
-		databaseShow : false,
-		database : {
-			name : '',
-			url : '',
-			dbType : '',
-			lastTestTime : ''
+		datasourceItem: [],
+		databaseShow: false,
+		database: {
+			name: '',
+			url: '',
+			dbType: '',
+			lastTestTime: ''
 		}
 	},
-	methods : {
-		query : function() {
+	methods: {
+		query: function () {
 			if (vm.q.id === '') {
 				vm.$message.warning("请选择要查询的数据源！");
 				return;
@@ -25,40 +25,40 @@ var vm = new Vue({
 			Ajax.request({
 				url: vm.baseUrl + '/' + vm.q.id,
 				async: true,
-				successCallback: function(r) {
+				successCallback: function (r) {
 					vm.databaseShow = true;
 					vm.reload(r.data);
 				}
 			});
 		},
-		reload : function (data) {
+		reload: function (data) {
 			vm.parseDatabase(data.databaseDO);
 			var tableList = data.databaseTableDOList;
 			if (tableList.length > 0) {
 				$("#doc").html("");
-				for (var i = 0 ; i < tableList.length; i++) {
+				for (var i = 0; i < tableList.length; i++) {
 					var table = tableList[i];
 					var html = "";
 					html += '<fieldset class="layui-elem-field layui-field-title">';
-					html +=		'<legend><a name="' + table.name + '">' + table.name + ' - ' + table.remark + '</a></legend>';
-					html +=	'</fieldset>';
+					html += '<legend><a name="' + table.name + '">' + table.name + ' - ' + table.remark + '</a></legend>';
+					html += '</fieldset>';
 					html += '<table lay-filter="databaseTable_' + i + '" lay-size="sm">';
-					html += 	'<thead>';
-					html += 		'<tr>';
-					html += 			'<th lay-data="{field:\'remark\', width:140}">字段名称</th>';
-					html += 			'<th lay-data="{field:\'name\', width:160}">字段标识</th>';
-					html += 			'<th lay-data="{field:\'type\', width:120}">字段类型</th>';
-					html += 			'<th lay-data="{field:\'dataLength\', width:80, align : \'center\'}">长度</th>';
-					html += 			'<th lay-data="{field:\'propertyName\', width:120}">实体字段</th>';
-					html += 			'<th lay-data="{field:\'propertyType\', width:100}">实体类型</th>';
-					html += 			'<th lay-data="{field:\'nullable\', width:100, align : \'center\'}">允许空值</th>';
-					html += 			'<th lay-data="{field:\'keyFlag\', width:70, align : \'center\'}">主键</th>';
-					html += 		'</tr>';
-					html += 	'</thead>';
-					html += 	'<tbody>';
+					html += '<thead>';
+					html += '<tr>';
+					html += '<th lay-data="{field:\'remark\', width:140}">字段名称</th>';
+					html += '<th lay-data="{field:\'name\', width:160}">字段标识</th>';
+					html += '<th lay-data="{field:\'type\', width:120}">字段类型</th>';
+					html += '<th lay-data="{field:\'dataLength\', width:80, align : \'center\'}">长度</th>';
+					html += '<th lay-data="{field:\'propertyName\', width:120}">实体字段</th>';
+					html += '<th lay-data="{field:\'propertyType\', width:100}">实体类型</th>';
+					html += '<th lay-data="{field:\'nullable\', width:100, align : \'center\'}">允许空值</th>';
+					html += '<th lay-data="{field:\'keyFlag\', width:70, align : \'center\'}">主键</th>';
+					html += '</tr>';
+					html += '</thead>';
+					html += '<tbody>';
 					var fieldList = table.databaseTableFieldDOList;
 					if (fieldList.length > 0) {
-						for (var j = 0 ; j < fieldList.length; j++) {
+						for (var j = 0; j < fieldList.length; j++) {
 							var field = fieldList[j];
 							html += '<tr>';
 							html += '<td>' + field.remark + '</td>';
@@ -68,11 +68,11 @@ var vm = new Vue({
 							html += '<td>' + field.propertyName + '</td>';
 							html += '<td>' + field.propertyType + '</td>';
 							html += '<td>' + (field.nullable === "1" ? '<span class="layui-badge layui-bg-blue">是</span>' : '<span class="layui-badge layui-bg-orange">否</span>') + '</td>';
-							html += '<td>' + (field.keyFlag  === "1" ? '<i class="fa fa-key fa-rotate-90"></i>' : '') + '</td>';
+							html += '<td>' + (field.keyFlag === "1" ? '<i class="fa fa-key fa-rotate-90"></i>' : '') + '</td>';
 							html += '</tr>';
 						}
 					}
-					html += 	'</tbody>';
+					html += '</tbody>';
 					html += '</table>';
 					$("#doc").append(html);
 					layui.table.init('databaseTable_' + i, {
@@ -82,7 +82,7 @@ var vm = new Vue({
 				vm.content(tableList);
 			}
 		},
-		content : function (tableList) {
+		content: function (tableList) {
 			var html = "";
 			for (var i = 0; i < tableList.length; i++) {
 				var table = tableList[i];
@@ -90,31 +90,31 @@ var vm = new Vue({
 			}
 			$(".site-dir").html(html);
 			layui.layer.open({
-			  type: 1,
-			  content: $("#contentDialog"),
-			  skin: 'layui-layer-dir',
-			  area: 'auto',
-			  maxHeight: $(window).height() - 300,
-			  title : '目录',
-			  offset: 'r',
-			  shade: false,
-			  success: function(layero, index){
-		          layer.style(index, {
-		            marginLeft: -15
-		          });
-		        }
+				type: 1,
+				content: $("#contentDialog"),
+				skin: 'layui-layer-dir',
+				area: 'auto',
+				maxHeight: $(window).height() - 300,
+				title: '目录',
+				offset: 'r',
+				shade: false,
+				success: function (layero, index) {
+					layer.style(index, {
+						marginLeft: -15
+					});
+				}
 			});
 		},
-		parseDatabase : function (databaseDO) {
+		parseDatabase: function (databaseDO) {
 			vm.database = databaseDO;
 			vm.database.lastTestTime = constant.transDate(databaseDO.lastTestTime);
 		},
-		queryDatabaseDoc : function () {
+		queryDatabaseDoc: function () {
 			confirm('确认要重新拉取数据吗？', function () {
 				Ajax.request({
 					url: vm.baseUrl + '/pull/' + vm.q.id,
 					async: true,
-					successCallback: function(r) {
+					successCallback: function (r) {
 						alert("拉取成功！", function () {
 							vm.query();
 						});
@@ -122,28 +122,25 @@ var vm = new Vue({
 				});
 			});
 		},
-		handleReset : function(name) {
+		handleReset: function (name) {
 			handleResetForm(vm, name);
 		}
 	},
-	created: function() {
+	created: function () {
 		Ajax.request({
 			url: '/develop/databases',
 			async: true,
 			type: 'GET',
-			successCallback: function(r) {
+			successCallback: function (r) {
 				if (r.data.length > 0) {
 					for (var i = 0; i < r.data.length; i++) {
 						var database = r.data[i];
-						if (i == 0) {
-							vm.q.id = database.id;
-						}
 						var obj = {
-								label : database.name,
-								value : database.id
+							label: database.name,
+							value: database.id
 						}
 						vm.datasourceItem.push(obj);
-						vm.query();
+						// vm.query();
 					}
 				}
 			}
@@ -151,6 +148,6 @@ var vm = new Vue({
 	}
 });
 
-layui.use(['table'], function() {
+layui.use(['table'], function () {
 	var table = layui.table;
 });
--- xhz-gen/src/main/java/com/xhz/CustomDbOracleQuery.java
+++ xhz-gen/src/main/java/com/xhz/CustomDbOracleQuery.java
@@ -9,6 +9,7 @@ import com.baomidou.mybatisplus.generator.config.querys.OracleQuery;
 
 /**
  * 自定义mysql的查询实现
+ * 
  * @author zhangzm
  *
  */
@@ -26,18 +27,18 @@ public class CustomDbOracleQuery extends OracleQuery implements IDbQuery {
 
 	@Override
 	public String tableFieldsSql() {
-		return "SELECT CASE WHEN A.DATA_TYPE = 'VARCHAR2' THEN A.DATA_LENGTH ELSE NULL END \"DataLength\", "
+		return "SELECT CASE WHEN A.DATA_TYPE = 'VARCHAR2' OR A .DATA_TYPE = 'CHAR' THEN A.DATA_LENGTH ELSE NULL END \"DataLength\", "
 				+ "CASE WHEN A.NULLABLE = 'N' THEN 'NO' WHEN A.NULLABLE = 'Y' THEN 'YES' ELSE A.NULLABLE END \"Null\", "
 				+ "A.COLUMN_NAME, CASE WHEN A.DATA_TYPE='NUMBER' THEN "
-	            + "(CASE WHEN A.DATA_PRECISION IS NULL THEN A.DATA_TYPE "
-	            + "WHEN NVL(A.DATA_SCALE, 0) > 0 THEN A.DATA_TYPE||'('||A.DATA_PRECISION||','||A.DATA_SCALE||')' "
-	            + "ELSE A.DATA_TYPE||'('||A.DATA_PRECISION||')' END) "
-	            + "ELSE A.DATA_TYPE END DATA_TYPE, B.COMMENTS,DECODE(C.POSITION, '1', 'PRI') KEY "
-	            + "FROM ALL_TAB_COLUMNS A "
-	            + " INNER JOIN ALL_COL_COMMENTS B ON A.TABLE_NAME = B.TABLE_NAME AND A.COLUMN_NAME = B.COLUMN_NAME AND B.OWNER = '#schema'"
-	            + " LEFT JOIN ALL_CONSTRAINTS D ON D.TABLE_NAME = A.TABLE_NAME AND D.CONSTRAINT_TYPE = 'P' AND D.OWNER = '#schema'"
-	            + " LEFT JOIN ALL_CONS_COLUMNS C ON C.CONSTRAINT_NAME = D.CONSTRAINT_NAME AND C.COLUMN_NAME=A.COLUMN_NAME AND C.OWNER = '#schema'"
-	            + " WHERE A.OWNER = '#schema' AND A.TABLE_NAME = '%s' ORDER BY A.COLUMN_ID ";
+				+ "(CASE WHEN A.DATA_PRECISION IS NULL THEN A.DATA_TYPE "
+				+ "WHEN NVL(A.DATA_SCALE, 0) > 0 THEN A.DATA_TYPE||'('||A.DATA_PRECISION||','||A.DATA_SCALE||')' "
+				+ "ELSE A.DATA_TYPE||'('||A.DATA_PRECISION||')' END) "
+				+ "ELSE A.DATA_TYPE END DATA_TYPE, B.COMMENTS,DECODE(C.POSITION, '1', 'PRI') KEY "
+				+ "FROM ALL_TAB_COLUMNS A "
+				+ " INNER JOIN ALL_COL_COMMENTS B ON A.TABLE_NAME = B.TABLE_NAME AND A.COLUMN_NAME = B.COLUMN_NAME AND B.OWNER = '#schema'"
+				+ " LEFT JOIN ALL_CONSTRAINTS D ON D.TABLE_NAME = A.TABLE_NAME AND D.CONSTRAINT_TYPE = 'P' AND D.OWNER = '#schema'"
+				+ " LEFT JOIN ALL_CONS_COLUMNS C ON C.CONSTRAINT_NAME = D.CONSTRAINT_NAME AND C.COLUMN_NAME=A.COLUMN_NAME AND C.OWNER = '#schema'"
+				+ " WHERE A.OWNER = '#schema' AND A.TABLE_NAME = '%s' ORDER BY A.COLUMN_ID ";
 	}
 
 	@Override
@@ -77,6 +78,6 @@ public class CustomDbOracleQuery extends OracleQuery implements IDbQuery {
 
 	@Override
 	public String[] fieldCustom() {
-		return new String[]{"Null", "DataLength"};
+		return new String[] { "Null", "DataLength" };
 	}
 }
