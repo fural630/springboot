b22664852da3efcfa25f40c17e4f4540e1a81df8
fural630 <312651213@qq.com>
2019-02-20 10:48:20 +0800

    完善shiro框架配置

--- xhz-front/src/main/java/com/xhz/config/ShrioConfig.java
+++ xhz-front/src/main/java/com/xhz/config/ShrioConfig.java
@@ -1,16 +1,23 @@
 package com.xhz.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.realm.text.TextConfigurationRealm;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
-import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
-import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile({"pro", "test", "dev"})
 public class ShrioConfig {
 
 	@Bean
@@ -29,23 +36,60 @@ public class ShrioConfig {
 		return realm;
 	}
 
+	@Bean
+	public ShiroFilterFactoryBean shiroFilterFactoryBean() {
+		ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
+		shiroFilter.setSecurityManager(defaultWebSecurityManager());
+		shiroFilter.setLoginUrl("/login.html");
+		shiroFilter.setSuccessUrl("/index.html");
+		Map<String, String> filterChainDefinitionMap = new HashMap<String, String>();
+		// 登录请求，允许访问
+		filterChainDefinitionMap.put("/sys/login", "anon");
+		// 静态资源匿名访问
+		filterChainDefinitionMap.put("/css/**", "anon");
+		filterChainDefinitionMap.put("/font/**", "anon");
+		filterChainDefinitionMap.put("/img/**", "anon");
+		filterChainDefinitionMap.put("/js/**", "anon");
+		filterChainDefinitionMap.put("/plugins/**", "anon");
+		// 退出请求，不需要配置action
+		filterChainDefinitionMap.put("/logout", "logout");
+		// 拦截其他所有请求，都需要登录允许
+		filterChainDefinitionMap.put("/**", "authc");
+		shiroFilter.setFilterChainDefinitionMap(filterChainDefinitionMap);
+		return shiroFilter;
+	}	
 
 	@Bean
-	public ShiroFilterChainDefinition shiroFilterChainDefinition() {
-		DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
-		// 登录请求，允许访问
-		chainDefinition.addPathDefinition("/sys/login", "anon");
-		// 静态资源匿名访问
-		chainDefinition.addPathDefinition("/css/**", "anon");
-		chainDefinition.addPathDefinition("/font/**", "anon");
-		chainDefinition.addPathDefinition("/img/**", "anon");
-		chainDefinition.addPathDefinition("/js/**", "anon");
-		chainDefinition.addPathDefinition("/plugins/**", "anon");
-		// 退出请求，不需要配置action
-		chainDefinition.addPathDefinition("/logout", "logout");
-		// 拦截其他所有请求，都需要登录允许
-		chainDefinition.addPathDefinition("/**", "authc");
-		return chainDefinition;
+	public DefaultWebSessionManager defaultWebSessionManager() {
+		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+		// 设置session过期时间为60分钟(单位：毫秒)，1分钟为60000，默认为30分钟
+		sessionManager.setGlobalSessionTimeout(60000 * 60);
+		sessionManager.setSessionValidationSchedulerEnabled(true);
+		sessionManager.setSessionIdUrlRewritingEnabled(false);
+		return sessionManager;
+	}
+	
+	@Bean 
+	public DefaultWebSecurityManager defaultWebSecurityManager() {
+		DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
+		defaultWebSecurityManager.setSessionManager(defaultWebSessionManager());
+		defaultWebSecurityManager.setRealm(realm());
+		return defaultWebSecurityManager;
+	}
+	
+	
+	@Bean
+	public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+		DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+		defaultAdvisorAutoProxyCreator.setProxyTargetClass(true);
+		return defaultAdvisorAutoProxyCreator;
 	}
 
+	@Bean
+	@DependsOn("lifecycleBeanPostProcessor")
+	public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor () {
+		AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+		authorizationAttributeSourceAdvisor.setSecurityManager(defaultWebSecurityManager());
+		return authorizationAttributeSourceAdvisor;
+	}
 }
